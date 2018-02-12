9cd317a252d0e6331cb259a71df1798e6376662a
bentonwong <bentonwong@gmail.com>
2018-02-11 18:17:16 -0800

    add new team form

--- app/controllers/teams_controller.rb
+++ app/controllers/teams_controller.rb
@@ -4,4 +4,18 @@ class TeamsController < ApplicationController
     @teams = Team.all
   end
 
+  def new
+    @team = Team.new
+  end
+
+  def create
+    raise params.inspect
+  end
+
+  private
+
+  def team_params
+    params.require(:team).permit(:name, :start_day, :shift_length)
+  end
+
 end
