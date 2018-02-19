b08ded0c6accf460721e17527028d861efff448a
bentonwong <bentonwong@gmail.com>
2018-02-19 00:22:46 -0800

    start employee dashboard

--- app/controllers/employees_controller.rb
+++ app/controllers/employees_controller.rb
@@ -31,14 +31,21 @@ class EmployeesController < ApplicationController
 
   def dashboard
     @employee = current_user
-    #@team = Team.find_by_id(1)
-    #team_shift_ids = @team.collect_shift_ids_by_team
-    #@events_by_date = Day.where(shift_id: team_shift_ids, workday: true).group_by(&:value)
-    #@date = params[:date] ? Date.parse(params[:date]) : Date.today
+    @employee_form = Employee.new
+    @employee_team_select = {}
+    @employee.teams.each { |team| @employee_team_select[team.name] = team.id }
+    team_ids = params.dig(:employee, :team_ids) || @employee.teams.first.id
+    if team_ids
+      @team = Team.find_by_id(team_ids)
+      team_shift_ids = @team.collect_shift_ids_by_team
+      @events_by_date = Day.where(shift_id: team_shift_ids, workday: true).group_by(&:value)
+      @date = params[:date] ? Date.parse(params[:date]) : Date.today
+    end
     @next_shift_start_end_days = current_user.get_shift_start_end_day_values
   end
 
   def shift_details
+    raise params.inspect
   end
 
   private
