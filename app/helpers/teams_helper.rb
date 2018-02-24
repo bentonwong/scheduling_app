e7ed60e7aaca72b41e49c95dbe3aa6874e9c5d7c
bentonwong <bentonwong@gmail.com>
2018-02-23 23:27:11 -0800

    backup

--- app/helpers/teams_helper.rb
+++ app/helpers/teams_helper.rb
@@ -8,12 +8,8 @@ module TeamsHelper
     team.start_day_as_string
   end
 
-  def weekday_names
-    DayPreference.workday_list
-  end
-
-  def day_of_the_week(preference_day)
-    DayPreference.weekday_key_value_name[preference_day]
+  def team_days_of_the_week
+    Team.days_of_the_week
   end
 
 end
